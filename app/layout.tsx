b497f5b21498a58e360cbdfcd1cca13c4f68911e
Nganaremba <nganaremba-rem>
2024-05-27 17:00:31 +0530

    changed logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,16 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.png" type="image/png" sizes="192x192" />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon.png"
+          type="image/png"
+          sizes="192x192"
+        />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
